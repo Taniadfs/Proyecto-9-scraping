2a38ece09f9c21ec21bb37c152b41561b5c7aa38
Taniadfs <t.dangelofonfria@gmail.com>
2026-04-28 21:35:07 +0200

    cambio a fnac con manejo de modales reales

--- scraper.js
+++ scraper.js
@@ -11,11 +11,10 @@ const scrape = async () => {
     await connectDB()
     console.log('Conectado a MongoDB')
 
-    browser = await puppeteer.launch()
-    console.log('Navegador abierto')
+    browser = await puppeteer.launch({ headless: false })
 
     const page = await browser.newPage()
-    await page.goto('https://www.fnac.com/')
+    await page.goto ('https://www.fnac.com/l55983/Nouveautes-Poche')
     console.log('Página cargada')
 
     try {
@@ -28,9 +27,16 @@ const scrape = async () => {
     
 
     const allBooks = []
-    let nextButton = true
 
-    while (nextButton) {
+    for (let i= 1; i <= 25; i++) {
+
+      const url = i === 1
+    ? 'https://www.fnac.com/l55983/Nouveautes-Poche'
+    : `https://www.fnac.com/l55983/Nouveautes-Poche?PageIndex=${i}`
+
+
+  await page.goto(url)
+  console.log(`Scrapeando página ${i}`)
 
       try {
         await page.waitForSelector(' #batchsdk-ui-alert__buttons_negative', { timeout: 5000 })
@@ -38,26 +44,23 @@ const scrape = async () => {
         console.log('Modal de abonarse cerrado')
       } catch (e) {
         console.log('No se encontró el modal de abonarse')}
-      const books = await page.evaluate(() => {
-        const bookElements = document.querySelectorAll('article.product_pod')
-        return Array.from(bookElements).map((book) => {
-          const title = book.querySelector('h3 a').getAttribute('title')
-          const price = book.querySelector('.price_color').textContent
-          const image =
-            'https://books.toscrape.com/' +
-            book.querySelector('img').getAttribute('src')
-          return { title, price, image }
+    
+        await page.waitForSelector('article.Article-itemGroup', { timeout: 10000 })
+        const books = await page.evaluate(() => {
+          const bookElements = document.querySelectorAll('article.Article-itemGroup')
+          return Array.from(bookElements).map((book) => {
+            const img = book.querySelector('img.Article-itemVisualImg')
+            if (!img) return null  // si no hay imagen, ignora este producto
+            
+            const title = img.getAttribute('alt')
+            const price = book.querySelector('div.Article-price')?.textContent.trim()
+            const image = img.getAttribute('src')
+            return { title, price, image }
+          }).filter(Boolean) 
         })
-      })
 
-      nextButton = await page.$('li.next a')
-      if (nextButton) {
-      await Promise.all([
-       nextButton.click(),
-     page.waitForNavigation()
-      ])
-      }
-
+    
+        await new Promise(resolve => setTimeout(resolve, 3000))
       allBooks.push(...books)
     }
 
